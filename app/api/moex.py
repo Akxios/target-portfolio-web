ecbc014564079432dd42218c1fb7d78308753ede
Akxios <artemermolaev466@gmail.com>
2026-01-26 20:34:01 +0900

    Update: moex, rename ticket -> ticker

--- app/api/moex.py
+++ app/api/moex.py
@@ -8,12 +8,12 @@ from app.services.moex import get_bond, get_share, search_instruments
 router = APIRouter(prefix="/api/moex", tags=["MOEX"])
 
 
-@router.get("/shares/{ticket}", response_model=ShareOut)
+@router.get("/shares/{ticker}", response_model=ShareOut)
 async def get_share_data(ticker: str):
     return await get_share(ticker)
 
 
-@router.get("/bonds/{ticket}", response_model=BondOut)
+@router.get("/bonds/{ticker}", response_model=BondOut)
 async def get_bond_data(ticker: str):
     return await get_bond(ticker)
 
