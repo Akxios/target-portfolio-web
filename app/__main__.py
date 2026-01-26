748ca7d5e9a63801a6982951d9c8f2d4d614fd31
Akxios <artemermolaev466@gmail.com>
2026-01-26 11:47:20 +0900

    Fix: field main.py

--- app/__main__.py
+++ app/__main__.py
@@ -5,9 +5,10 @@ def main():
     uvicorn.run(
         "app.main:app",
         host="127.0.0.1",
-        port="8000",
+        port=8000,
         reload=True,
     )
+
 
 if __name__ == "__main__":
     main()
