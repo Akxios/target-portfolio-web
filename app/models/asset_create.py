7ac8acbd621fac5d41176717ada10af7f7dfbd12
Akxios <artemermolaev466@gmail.com>
2026-01-26 20:07:43 +0900

    Fix: asset_create

--- app/models/asset_create.py
+++ app/models/asset_create.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel, Field
 
+
 class AssetCreate(BaseModel):
-    ticker: str = Field(..., example="SBER")
+    ticker: str = Field(..., examples=["SBER"])
     target_qty: int = Field(..., ge=0)
     current_qty: int = Field(0, ge=0)
