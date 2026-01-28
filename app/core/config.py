a2f507f6377bf1049a2e16e0418fd5ef3d676359
Akxios <artemermolaev466@gmail.com>
2026-01-28 20:52:24 +0900

    Update: added base url

--- app/core/config.py
+++ app/core/config.py
@@ -1,17 +1,19 @@
 from pathlib import Path
-from pydantic_settings import  BaseSettings, SettingsConfigDict
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 BASE_DIR = Path(__file__).resolve().parents[2]
 
 
 class Settings(BaseSettings):
-    MOEX_BASE_URL: str
-    MONGODB_URL: str
-    MONGODB_DB_NAME: str
+    MOEX_BASE_URL: str = "https://iss.moex.com/iss/"
+    MONGODB_URL: str = "mongodb://localhost:27017"
+    MONGODB_DB_NAME: str = "portfolio"
 
     model_config = SettingsConfigDict(
         env_file=BASE_DIR / ".env",
         env_file_encoding="utf-8",
     )
+
 
 settings = Settings()
