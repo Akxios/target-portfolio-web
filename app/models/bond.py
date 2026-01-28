1dd9eea890487040d90435d1cb59340da4f3ee69
Akxios <artemermolaev466@gmail.com>
2026-01-28 20:55:06 +0900

    Update: more details fo field

--- app/models/bond.py
+++ app/models/bond.py
@@ -1,9 +1,11 @@
-from pydantic import BaseModel
+from typing import Optional
+
+from pydantic import BaseModel, Field
 
 
-class BondOut(BaseModel):
-    ticker: str
-    name: str
-    price: float | None
-    yield_percent: float | None
-    coupon_value: float | None
+class MoexBondOut(BaseModel):
+    ticker: str = Field(..., examples=["OFZ26238"])
+    name: str = Field(..., examples=["ОФЗ 26238"])
+    price: Optional[float] = Field(None, description="Текущая цена")
+    yield_percent: Optional[float] = Field(None, description="Доходность, % годовых")
+    coupon_value: Optional[float] = Field(None, description="Размер купона")
