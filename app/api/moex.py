0b1e6c0a22627b3613d88c15dea54eebcf0f2722
Akxios <artemermolaev466@gmail.com>
2026-01-28 20:43:28 +0900

    Update: New structure, union get_bond and get_share

--- app/api/moex.py
+++ app/api/moex.py
@@ -1,23 +1,21 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from pymoex.services.search import InstrumentType
 
-from app.models.bond import BondOut
-from app.models.share import ShareOut
-from app.services.moex import get_bond, get_share, search_instruments
+from app.services.moex import get_moex_bond, get_moex_share, search_moex_instruments
 
 router = APIRouter(prefix="/api/moex", tags=["MOEX"])
 
 
-@router.get("/shares/{ticker}", response_model=ShareOut)
-async def api_get_share_data(ticker: str):
-    return await get_share(ticker)
+@router.get("/instruments/{ticker}")
+async def api_get_instrument(ticker: str, type: InstrumentType):
+    if type == InstrumentType.SHARE:
+        return await get_moex_share(ticker)
+    elif type == InstrumentType.BOND:
+        return await get_moex_bond(ticker)
 
-
-@router.get("/bonds/{ticker}", response_model=BondOut)
-async def api_get_bond_data(ticker: str):
-    return await get_bond(ticker)
+    raise HTTPException(status_code=400, detail="Invalid instrument type")
 
 
 @router.get("/search")
-async def api_search(ticker: str, type: InstrumentType):
-    return await search_instruments(ticker, type)
+async def api_search_moex(ticker: str, type: InstrumentType):
+    return await search_moex_instruments(ticker, type)
