d5f2e92a04652abbd66fe0eff500db1a66a79319
Akxios <artemermolaev466@gmail.com>
2026-01-29 19:33:57 +0900

    Fix: prefix and added new router

--- app/main.py
+++ app/main.py
@@ -1,13 +1,15 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 from pymoex.exceptions import InstrumentNotFoundError
 from starlette.responses import JSONResponse
 
 from app.api.moex import router as api_moex_router
 from app.api.portfolio import router as api_portfolio_router
 from app.core.database import close_mongo_connection, connect_to_mongo
+from app.web.router import router as web_router
 
-app = FastAPI(title="Target Portfolio API", description="API")
+app = FastAPI(title="Target Portfolio API")
 
 origins = ["http://localhost", "http://localhost:8000", "http://127.0.0.1:8000"]
 
@@ -19,16 +21,20 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(api_moex_router)
-app.include_router(api_portfolio_router)
+# WEB
+app.include_router(web_router)
+
+# API
+app.include_router(api_portfolio_router, prefix="/api")
+app.include_router(api_moex_router, prefix="/api")
+
+# STATIC
+app.mount("/static", StaticFiles(directory="app/web/static"), name="static")
 
 
 @app.exception_handler(InstrumentNotFoundError)
 async def instrument_not_found_handler(request, exc: InstrumentNotFoundError):
-    return JSONResponse(
-        status_code=404,
-        content={"detail": str(exc)},
-    )
+    return JSONResponse(status_code=404, content={"detail": str(exc)})
 
 
 @app.on_event("startup")
@@ -39,8 +45,3 @@ async def startup():
 @app.on_event("shutdown")
 async def shutdown():
     await close_mongo_connection()
-
-
-@app.get("/")
-def root():
-    return {"status": "target-portfolio is running"}
