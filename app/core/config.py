7765ff8d9ffaf850c8d4f438ae30c86203738b21
Akxios <artemermolaev466@gmail.com>
2026-02-09 18:12:14 +0900

    Update(config.py): add new field cors_origins

--- app/core/config.py
+++ app/core/config.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from typing import List
 
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
@@ -9,10 +10,14 @@ class Settings(BaseSettings):
     MOEX_BASE_URL: str = "https://iss.moex.com/iss/"
     MONGODB_URL: str = "mongodb://localhost:27017"
     MONGODB_DB_NAME: str = "portfolio"
+    CORS_ORIGINS: List[str] = [
+        "http://localhost",
+        "http://localhost:8000",
+        "http://127.0.0.1:8000",
+    ]
 
     model_config = SettingsConfigDict(
-        env_file=BASE_DIR / ".env",
-        env_file_encoding="utf-8",
+        env_file=BASE_DIR / ".env", env_file_encoding="utf-8", extra="ignore"
     )
 
 
