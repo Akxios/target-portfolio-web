cda571d02f38e65cc5de14cd6ebd5f7ae2e5e66f
Akxios <artemermolaev466@gmail.com>
2026-02-18 20:01:15 +0900

    Update(models)

--- app/models/position.py
+++ app/models/position.py
@@ -1,8 +1,9 @@
+from beanie import Document
 from pydantic import BaseModel, Field
 from pymoex.models.enums import InstrumentType
 
 
-class Position(BaseModel):
+class Position(Document):
     ticker: str = Field(..., examples=["SBER"])
     name: str = Field(..., examples=["ОФЗ 26238"])
     short_name: str = Field(examples=["ОФЗ"])
@@ -10,6 +11,12 @@ class Position(BaseModel):
 
     target_qty: int = Field(..., ge=0)
     current_qty: int = Field(..., ge=0)
+
+    class Settings:
+        name = "portfolio"
+        indexes = [
+            "ticker",
+        ]
 
 
 class PositionCreate(BaseModel):
